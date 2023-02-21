cdce9cf4226d7f2f58bfecfbb5137d8c9e6e27ca
0x580x540x43 <78674390+0x580x540x43@users.noreply.github.com>
2023-02-20 19:37:25 -0500

    Update NCT.lua

--- NCT.lua
+++ NCT.lua
@@ -23,6 +23,7 @@ local function Main()
     if tonumber(Currency.Text) >= 22550 then
         ReplicatedStorage.Packages._Index["sleitnick_knit@1.4.7"].knit.Services.ShopService.RE.PurchaseItem:FireServer("Accessories", "008")
         ReplicatedStorage.Packages._Index["sleitnick_knit@1.4.7"].knit.Services.ShopService.RE.PurchaseItem:FireServer("Accessories", "007")
+        return
     else
         queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/0x580x540x43/Freebies/main/NCT.lua"))
         game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,game.JobId, LocalPlayer)
